1be08429562302fb2be4d5ef70f9c0b8f41f1940
EneAndrei789 <andreileonardene@gmail.com>
2024-12-08 18:17:52 +0200

    commit
    gg

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+    // Add imports above this line
+    import { galleryItems } from './gallery-items';
+    // Change code below this line
 
-console.log(galleryItems);
+    console.log(galleryItems);
+
+
+    import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+// Selectează lista din DOM
+const galleryList = document.querySelector('.gallery');
+
+// Generează marcajul HTML pentru galerie
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`).join('');
+
+// Inserează marcajul în DOM
+galleryList.innerHTML = galleryMarkup;
+
+// Inițializează SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
